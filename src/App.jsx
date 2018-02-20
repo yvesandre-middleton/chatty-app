e24d65f5a166f7bbfae09f9807c6a0f53fe621dd
yvesandre-middleton <yvesmiddleton@gmail.com>
2018-02-20 17:18:48 +0000

    added connection to websockets server

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,32 @@ class App extends Component {
       ]
     };
   }
+
+  inputText = (message) => {
+    this.setState({
+      messages: this.state.messages.concat([{
+        username: message.username,
+        content: message.content
+      }])
+    })
+  }
+
+  componentDidMount() {
+
+    this.webSocket = new WebSocket("ws://localhost:3001");
+    window.mySocket = this.webSocket;
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
   render() {
     console.log("Rendering <App/>");
     return (
@@ -32,7 +58,7 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages={this.state.messages}/>
-        <ChatBar currentUser={this.state.currentUser}/>
+        <ChatBar currentUser={this.state.currentUser} messages={this.state.messages} onSubmit={this.inputText}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,14 +2,50 @@ import React, {Component} from 'react';
 import MessageList from './MessageList.jsx';
 
 class ChatBar extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: this.props.currentUser.name,
+      message: ''
+    }
+  }
+
+  _usernameChanged = e => {
+    this.setState({ username: e.target.value });
+  };
+
+  _messageChanged = e => {
+    this.setState({ message: e.target.value });
+  };
+
   render() {
     console.log("Rendering <ChatBar/>");
     return (
       <footer className="chatbar">
-      <input  className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser.name}/>
-      <input  className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input  className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser.name} onChange={this._usernameChanged}/>
+        <input  className="chatbar-message" placeholder="Type a message and hit ENTER"
+          onChange={this._messageChanged}
+          onKeyPress={e => {
+            if (e.key === 'Enter') {
+              this._submitQuery();
+            }
+          }}
+        />
+      {/*<button onClick = {this.inputText.bind(this)}></button>*/}
       </footer>
     );
   }
-}
+
+  _submitQuery = () => {
+    this.props.onSubmit(
+      {
+        content: this.state.message,
+        username: this.state.username
+      }
+    );
+  }
+};
+
 export default ChatBar;
